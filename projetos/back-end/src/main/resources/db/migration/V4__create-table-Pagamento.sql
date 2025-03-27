6df8ef02d8a3fda3c930a8c9ecd2414edb3ea19e
Arthur-MARKOWICZ <164639904+Arthur-MARKOWICZ@users.noreply.github.com>
2025-03-27 10:52:04 -0300

    corregindo erro na migration pagamento

--- projetos/back-end/src/main/resources/db/migration/V4__create-table-Pagamento.sql
+++ projetos/back-end/src/main/resources/db/migration/V4__create-table-Pagamento.sql
@@ -1,6 +1,8 @@
 create table pagamento(
     id bigint not null auto_increment,
+    pedido_id bigint not null,
     metodo_pagamento varchar(100) not null,
     valor double not null,
+    FOREIGN KEY (pedido_id) REFERENCES pedido(id),
     primary key(id)
 );
