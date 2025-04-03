736beb3d445a81a239a1f02f7b32470bf595f883
Luigi-Morgenstern-Milani <luigimilani2003@outlook.com>
2025-04-03 10:29:42 -0300

    Utualizando navbar pra aparecer em todas as paginas

--- projetos/front-end/navbar.js
+++ projetos/front-end/navbar.js
@@ -25,3 +25,11 @@ class MobileNavbar {
 
 const mobileNavbar = new MobileNavbar(".mobile-menu", ".navlist");
 mobileNavbar.init();
+document.addEventListener("DOMContentLoaded", function() {
+    fetch("navbar.html")
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById("navbar-container").innerHTML = data;
+        })
+        .catch(error => console.error("Erro ao carregar navbar:", error));
+});
