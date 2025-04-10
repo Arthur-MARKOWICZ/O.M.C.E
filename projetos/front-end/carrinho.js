8def9cec9a71a7f3a4fd61775aa193b436494a4d
GustavoTF06 <gustavotissot06@gmail.com>
2025-04-10 10:35:51 -0300

    alterando carrinho para section
    continuando o carrinho e mudando de BD para uma section com local storage/ Não testado ainda

--- projetos/front-end/carrinho.js
+++ projetos/front-end/carrinho.js
@@ -1,4 +1,41 @@
-const parametro = new URLSearchParams(window.location.search);
-const produtoId = parametro.get('id'); 
-console.log(produtoId);
-fetch(`http://localhost:8080/produto/visualizarDetalhesProduto/${produtoId}`)
+let carrinho = [];
+
+window.onload = function() {
+  const salvarCarrinho = localStorage.getItem('Carrinho');
+  if (salvarCarrinho) {
+    carrinho = JSON.parse(salvarCarrinho);
+    atualizarCarrinho();
+  }
+};
+
+function adicionarProduto(nome, preco, id) {
+  carrinho.push({ nome: nome, preco: preco, id: id });
+  salvarCarrinho();
+  atualizarCarrinho();
+}
+
+function atualizarCarrinho() {
+  const itensCarrinho = document.getElementById('');
+  const valorTotal = document.getElementById('');
+  itensCarrinho.innerHTML = '';
+
+  let total = 0;
+  carrinho.forEach(item => {
+    const li = document.createElement('li');
+    li.textContent = `${produto.nome} - R$ ${produto.preco.toFixed(2)}`;
+    itensCarrinho.appendChild(li);
+    total += produto.preco;
+  });
+
+  valorTotal.textContent = total.toFixed(2);
+}
+
+function limparCarrinho() {
+  carrinho = [];
+  localStorage.removeItem('');
+  updateCartDisplay();
+}
+
+function salvarCarrinho() {
+  localStorage.setItem('', JSON.stringify(carrinho));
+}
