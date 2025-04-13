338d4536b4e0aaf70a77a3b4b78d523fb003ca32
Arthur-MARKOWICZ <164639904+Arthur-MARKOWICZ@users.noreply.github.com>
2025-04-13 17:43:31 -0300

    criando uma validacao de produto para cadastro e  alterar no back-end

---
+++ projetos/back-end/src/main/java/OMCE/OMCE/Validacao/ValidacaoProduto.java
@@ -0,0 +1,42 @@
+package OMCE.OMCE.Validacao;
+
+import OMCE.OMCE.Produto.DadosAlterarDadosProduto;
+import OMCE.OMCE.Produto.DadosCadastroProduto;
+
+public class ValidacaoProduto {
+    public void ValidarCadastroProduto(DadosCadastroProduto dados){
+        if(dados.nome() == null){
+            throw new RuntimeException("O produto nao tem nome,adicionar o nome");
+        }
+        if(dados.detalhes() == null){
+            throw  new RuntimeException("O detalhes do produto esta em branco");
+        }
+        if(dados.imagem() == null){
+            throw  new RuntimeException("O produto esta sem imagem");
+        }
+        if(dados.imagem_tipo() == null){
+            throw  new RuntimeException("O produto esta sem imagem");
+        }
+        if(dados.preco() <= 0){
+            throw new RuntimeException("O produto esta com um preco invalido");
+        }
+    }
+    public void ValidarAlterarProduto(DadosAlterarDadosProduto dados){
+        if(dados.nome() == null){
+            throw new RuntimeException("O produto nao tem nome,adicionar o nome");
+        }
+        if(dados.detalhes() == null){
+            throw  new RuntimeException("O detalhes do produto esta em branco");
+        }
+        if(dados.imagem() == null){
+            throw  new RuntimeException("O produto esta sem imagem");
+        }
+        if(dados.imagem_tipo() == null){
+            throw  new RuntimeException("O produto esta sem imagem");
+        }
+        if(dados.preco() <= 0){
+            throw new RuntimeException("O produto esta com um preco invalido");
+        }
+    }
+
+}
