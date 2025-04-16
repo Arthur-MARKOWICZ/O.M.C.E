296a7b7c23d6e971a5ff8f292105b4f6cee2fa4e
Arthur-MARKOWICZ <164639904+Arthur-MARKOWICZ@users.noreply.github.com>
2025-04-16 15:46:43 -0300

    arrunado detalhes no spring security

--- projetos/back-end/src/main/java/OMCE/OMCE/config/SecurityFilter.java
+++ projetos/back-end/src/main/java/OMCE/OMCE/config/SecurityFilter.java
@@ -20,23 +20,32 @@ public class SecurityFilter  extends OncePerRequestFilter {
     TokenService tokenService;
     @Autowired
     UserRepository userRepository;
+
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
                                     FilterChain filterChain)
             throws ServletException, IOException {
         var token = this.recoverToken(request);
-        if(token !=null){
-        var email = tokenService.validateToken(token);
-            UserDetails user = userRepository.findByEmail(email);
-            var authentication = new UsernamePasswordAuthenticationToken(user,null, user.getAuthorities());
-            SecurityContextHolder.getContext().setAuthentication(authentication);
+        if (token != null) {
+            var email = tokenService.validateToken(token);
+            if (email != null) {
+                UserDetails user = userRepository.findByEmail(email);
+                if (user != null) {
+                    UsernamePasswordAuthenticationToken authentication =
+                            new UsernamePasswordAuthenticationToken(user, null, user.getAuthorities());
+                    SecurityContextHolder.getContext().setAuthentication(authentication);
+                }
+            }
         }
-        filterChain.doFilter(request,response);
+
+        filterChain.doFilter(request, response);
     }
 
     private String recoverToken(HttpServletRequest request) {
         var authHeader = request.getHeader("Authorization");
-        if(authHeader == null){return null;
+        if (authHeader == null) {
+            return null;
+        }
+        return authHeader.replace("Bearer ", "");
     }
-        return authHeader.replace("Bearer ","");
-}}
+}
