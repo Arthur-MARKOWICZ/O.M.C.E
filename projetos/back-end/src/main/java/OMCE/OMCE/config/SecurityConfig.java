7d2e8d35177233c3641ed9097dfbe11f38ccad4a
Arthur-MARKOWICZ <164639904+Arthur-MARKOWICZ@users.noreply.github.com>
2025-04-25 18:21:52 -0300

    adicionado o Swagger para criar uma documentacao automatica do endpoint do back

--- projetos/back-end/src/main/java/OMCE/OMCE/config/SecurityConfig.java
+++ projetos/back-end/src/main/java/OMCE/OMCE/config/SecurityConfig.java
@@ -42,6 +42,7 @@ public class SecurityConfig {
                 .authorizeHttpRequests(authorize -> authorize
                         .requestMatchers(HttpMethod.POST, "/auth/cadastro").permitAll()
                         .requestMatchers(HttpMethod.POST, "/auth/login").permitAll()
+                        .requestMatchers("/v3/api-docs/**","/swagger-ui.html","swagger-ui/**").permitAll()
                         .requestMatchers(HttpMethod.GET,"/produto/visualizarDetalhesProduto/*").permitAll()
                         .requestMatchers(HttpMethod.GET,"/produto/todos").permitAll()
                         .anyRequest().authenticated())
