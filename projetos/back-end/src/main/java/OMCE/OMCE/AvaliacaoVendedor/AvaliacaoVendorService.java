9880a604603ba5940fc1a4139cb25331d3aadb95
Arthur-MARKOWICZ <164639904+Arthur-MARKOWICZ@users.noreply.github.com>
2025-05-22 11:00:26 -0300

    criado o pegar media de avaliacao de vendedor e criado o pegar as avaliacoes do vendedor

--- projetos/back-end/src/main/java/OMCE/OMCE/AvaliacaoVendedor/AvaliacaoVendorService.java
+++ projetos/back-end/src/main/java/OMCE/OMCE/AvaliacaoVendedor/AvaliacaoVendorService.java
@@ -4,7 +4,11 @@ package OMCE.OMCE.AvaliacaoVendedor;
 import OMCE.OMCE.User.User;
 import OMCE.OMCE.User.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class AvaliacaoVendorService {
@@ -18,6 +22,18 @@ public class AvaliacaoVendorService {
         User vendedor = userRepository.getReferenceById(dto.vendedor_id());
         avaliacao.setVendedor(vendedor);
         repository.save(avaliacao);
+    }
+    public double mediaAvaliacao(Long id) {
+        List<Integer> avaliacao = repository.pegarTodasNotasAvaliacose(id);
+        double soma = avaliacao.stream()
+                .mapToDouble(Integer::doubleValue)
+                .sum();
 
+        double media = soma / avaliacao.size();
+        return media;
+    }
+    public Page<AvaliacaoVendedorRespostaDTO> pegarAvaliaca(Pageable pageable, Long id){
+        Page<AvaliacaoVendedor> avaliacoes = repository.findByVendedorId(id,pageable);
+        return avaliacoes.map(avaliacao -> new AvaliacaoVendedorRespostaDTO(avaliacao));
     }
 }
--- projetos/back-end/src/main/java/OMCE/OMCE/controller/AvaliacaoVendedorController.java
+++ projetos/back-end/src/main/java/OMCE/OMCE/controller/AvaliacaoVendedorController.java
@@ -1,13 +1,13 @@
 package OMCE.OMCE.controller;
 
 import OMCE.OMCE.AvaliacaoVendedor.AvaliacaoVendedorDTO;
+import OMCE.OMCE.AvaliacaoVendedor.AvaliacaoVendedorRespostaDTO;
 import OMCE.OMCE.AvaliacaoVendedor.AvaliacaoVendorService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/avaliacaoVendedor")
@@ -20,5 +20,15 @@ public class AvaliacaoVendedorController {
     public ResponseEntity cadastro(@RequestBody AvaliacaoVendedorDTO dto){
         service.criar(dto);
         return ResponseEntity.ok().build();
+    }
+    @GetMapping("/media/{id}")
+    public  ResponseEntity<Double> pegarMedia(@PathVariable Long id){
+        double media=  service.mediaAvaliacao(id);
+        return ResponseEntity.ok(media);
+    }
+    @GetMapping("/{id}")
+    public  ResponseEntity<Page<AvaliacaoVendedorRespostaDTO>> pegarAvaliacoes(@PathVariable Long id, Pageable pageable){
+        Page<AvaliacaoVendedorRespostaDTO> avaliacoes = service.pegarAvaliaca(pageable,id);
+        return ResponseEntity.ok(avaliacoes);
     }
 }
