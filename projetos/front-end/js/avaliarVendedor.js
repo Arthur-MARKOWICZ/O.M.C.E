c8916fbbb913f36686ce4e12e7ea972400694a2b
Arthur-MARKOWICZ <164639904+Arthur-MARKOWICZ@users.noreply.github.com>
2025-05-30 18:54:42 -0300

    colocando validacoes no campo de avaliacao

--- projetos/front-end/js/avaliarVendedor.js
+++ projetos/front-end/js/avaliarVendedor.js
@@ -2,17 +2,26 @@ const params = new URLSearchParams(window.location.search);
 const token = localStorage.getItem("jwt");
 const id_vendedor = params.get('vendedor');
 if (id_vendedor) {
-    // remove os parâmetros da URL
     window.history.replaceState({}, document.title, window.location.pathname);
 }
 const nota = document.getElementById("nota-avaliacao");
 const comentario = document.getElementById("Comentario");
+
 document.getElementById("form-avaliarVendedor").addEventListener("submit", async function(event) {
     event.preventDefault();
 const avaliacao ={
     nota: nota.value,
     comentario: comentario.value,
     vendedor_id: id_vendedor
+}
+if(nota.value == ""){
+    exibirErro("campo de nota e obrigatorio");
+    return;
+}
+if(comentario.value == ""){
+    exibirErro("campo de comentario e obrigatorio");
+        return;
+
 }
 const response = await fetch("http://localhost:8080/avaliacaoVendedor/cadastro", {
             method: "POST",
@@ -38,3 +47,12 @@ const response = await fetch("http://localhost:8080/avaliacaoVendedor/cadastro",
             event.target.reset();
             window.location.href = "../html/feed.html";
     }); 
+function exibirErro(mensagem, campo) {
+    Swal.fire({
+      title: "Não foi possível realizar seu cadastro",
+      text:"Erro no/a: " + mensagem,
+      icon:'warning',
+      confirmButtonText: 'OK'});("Erro no/a: " + mensagem);
+    campo.focus();
+    return false;
+}
