f976525b6baa9aa98fa96514d50215d0baa2c514
Your Name <arthurlopes25072005@gmail.com>
2025-09-27 08:12:09 -0300

    test: teste de integracao do produto repository e o test para salvar produto no banco

---
+++ projetos/back-end/src/test/java/OMCE/OMCE/integracao/ProdutoRepositoryTest.java
@@ -0,0 +1,54 @@
+package OMCE.OMCE.integracao;
+
+import OMCE.OMCE.Enderco.DadosEndereco;
+import OMCE.OMCE.Produto.Produto;
+import OMCE.OMCE.Produto.dto.DadosCadastroProduto;
+import OMCE.OMCE.Produto.repository.ProdutoRepository;
+
+import OMCE.OMCE.User.User;
+import OMCE.OMCE.User.dto.DadosCadastroUser;
+import OMCE.OMCE.User.repository.UserRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import static OMCE.OMCE.Produto.enums.Categoria.ESP32;
+import static OMCE.OMCE.Produto.enums.Condicao.USADO;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@ActiveProfiles("test")
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class ProdutoRepositoryTest {
+    @Autowired
+    private ProdutoRepository repository;
+    @Autowired
+    private UserRepository userRepository;
+    private User user;
+    @BeforeEach
+    void setup() {
+        repository.deleteAll();
+        userRepository.deleteAll();
+        DadosEndereco dadosEndereco = new DadosEndereco("8123434","brasil","test",
+                "test","Rua test");
+        DadosCadastroUser dadosCadastroUser = new DadosCadastroUser("test","12345678912"
+                ,"22-06-2005","test",dadosEndereco,"test@gmail","1231313139",
+                "testUser","test");
+        User usuario = new User(dadosCadastroUser);
+        user = userRepository.save(usuario);
+
+    }
+
+    @Test
+    void    deveSalvarProdutoNoBanco(){
+        DadosCadastroProduto dados = new DadosCadastroProduto("test",10,"test", user.getId(),
+                "10", "10",ESP32,USADO);
+        Produto produto = new Produto(dados);
+        produto.setUsuario(user);
+       var produtoBanco = repository.save(produto);
+        var produtoRetornadoBanco = repository.findById(produtoBanco.getId())
+                .orElseThrow();
+        assertEquals("test", produtoRetornadoBanco.getNome());
+    }
+}
