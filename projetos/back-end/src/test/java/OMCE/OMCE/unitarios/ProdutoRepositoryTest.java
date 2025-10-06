3b04014c4efc8294d5f24eb3978d5bfff8f991b3
André Benato <andregbenato@outlook.com>
2025-10-06 16:06:12 -0300

    Test: Pegar detalhes do produto pelo id

--- projetos/back-end/src/test/java/OMCE/OMCE/unitarios/ProdutoRepositoryTest.java
+++ projetos/back-end/src/test/java/OMCE/OMCE/unitarios/ProdutoRepositoryTest.java
@@ -1,6 +1,5 @@
 package OMCE.OMCE.unitarios;
 
-
 import OMCE.OMCE.Produto.Produto;
 import OMCE.OMCE.Produto.dto.DadosCadastroProduto;
 import OMCE.OMCE.Produto.repository.ProdutoRepository;
@@ -25,40 +24,64 @@ import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 public class ProdutoRepositoryTest {
+
     @Mock
     private ProdutoRepository repository;
+
     @Test
     @DisplayName("deve pegar os produtos pelo id")
-    void PegarProdutoPeloId(){
-        //Arrange
-        DadosCadastroProduto dados = new DadosCadastroProduto("test",10,"test",1l,
-                "10", "10",ESP32,USADO);
+    void PegarProdutoPeloId() {
+        // Arrange
+        DadosCadastroProduto dados = new DadosCadastroProduto(
+                "test", 10, "test", 1L,
+                "10", "10", ESP32, USADO
+        );
         Produto produto = new Produto(dados);
-        produto.setId(1l);
-        when(repository.findById(1l)).thenReturn(Optional.of(produto));
+        produto.setId(1L);
+        when(repository.findById(1L)).thenReturn(Optional.of(produto));
         // Act
-        Optional<Produto> result = repository.findById(1l);
+        Optional<Produto> result = repository.findById(1L);
         // Assert
         assertTrue(result.isPresent());
-        assertEquals("test",result.get().getNome());
+        assertEquals("test", result.get().getNome());
     }
+
+    @Test
+    @DisplayName("deve obter os detalhes do produto pelo id")
+    void DeveObterDetalhesProdutoPeloId() {
+        String imagemBase64 = "dGVzdA==";
+        String imagemTipo = "image/png";
+        DadosCadastroProduto dados = new DadosCadastroProduto(
+                "test", 10.0, "test", 1L,
+                imagemBase64, imagemTipo, ESP32, USADO
+        );
+        Produto produto = new Produto(dados);
+        produto.setId(1L);
+        when(repository.findById(1L)).thenReturn(Optional.of(produto));
+        Optional<Produto> result = repository.findById(1L);
+        assertTrue(result.isPresent());
+        assertEquals("test", result.get().getNome());
+        assertEquals("test", result.get().getDetalhes());
+    }
+
     @Test
     @DisplayName("deve pegar os produtos pelo id do usuario criador")
-    void PegarProdutosPorUsuario(){
-        //Arrange
+    void PegarProdutosPorUsuario() {
+        // Arrange
         Pageable pageable = PageRequest.of(0, 10);
-        DadosCadastroProduto dados = new DadosCadastroProduto("test",10,"test",1l,
-                "10", "10",ESP32,USADO);
+        DadosCadastroProduto dados = new DadosCadastroProduto(
+                "test", 10, "test", 1L,
+                "10", "10", ESP32, USADO
+        );
         Produto produto = new Produto(dados);
-        produto.setId(1l);
+        produto.setId(1L);
         Page<Produto> produtoPage = new PageImpl<>(List.of(produto));
-
-        when(repository.pegarProdutosUsuario(1l, pageable)).thenReturn(produtoPage);
+        when(repository.pegarProdutosUsuario(1L, pageable)).thenReturn(produtoPage);
         // Act
-        Page<Produto> result = repository.pegarProdutosUsuario(1l, pageable);
+        Page<Produto> result = repository.pegarProdutosUsuario(1L, pageable);
         // Assert
         assertTrue(result.isFirst());
-        assertEquals("test",result.getContent().get(0).getNome());
-        assertEquals(1l, result.getContent().get(0).getId());
+        assertEquals("test", result.getContent().get(0).getNome());
+        assertEquals(1L, result.getContent().get(0).getId());
     }
 }
