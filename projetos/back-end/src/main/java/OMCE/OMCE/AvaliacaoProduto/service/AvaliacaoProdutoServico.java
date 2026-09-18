9970a3e8153eec4a57a48bdd7c6fc946b75dd729
Luigi Morgenstern Milani <luigimilani2003@outlook.com>
2026-09-17 22:19:50 -0300

    resolvendo

--- projetos/back-end/src/main/java/OMCE/OMCE/AvaliacaoProduto/service/AvaliacaoProdutoServico.java
+++ projetos/back-end/src/main/java/OMCE/OMCE/AvaliacaoProduto/service/AvaliacaoProdutoServico.java
@@ -5,14 +5,18 @@ import OMCE.OMCE.AvaliacaoProduto.dto.AvaliacaoProdutoDTO;
 import OMCE.OMCE.AvaliacaoProduto.repository.AvaliacaoProdutoRepositorio;
 import OMCE.OMCE.Produto.Produto;
 import OMCE.OMCE.Produto.repository.ProdutoRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
+import OMCE.OMCE.Avaliacao.service.AvaliacaoTemplateService;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
-public class AvaliacaoProdutoServico {
+public class AvaliacaoProdutoServico
+        extends AvaliacaoTemplateService<AvaliacaoProdutoDTO, AvaliacaoProduto> {
 
     @Autowired
     private AvaliacaoProdutoRepositorio repositorio;
@@ -20,25 +24,30 @@ public class AvaliacaoProdutoServico {
     @Autowired
     private ProdutoRepository produtoRepositorio;
 
-    public void criar(AvaliacaoProdutoDTO dto) {
+    @Override
+    protected AvaliacaoProduto criarAvaliacao(AvaliacaoProdutoDTO dto) {
+
         Produto produto = produtoRepositorio.findById(dto.getIdProduto())
-            .orElseThrow(() -> new RuntimeException("Produto não encontrado"));
+                .orElseThrow(() ->
+                        new RuntimeException("Produto não encontrado"));
 
-        AvaliacaoProduto avaliacao = new AvaliacaoProduto(dto,produto);
+        return new AvaliacaoProduto(dto, produto);
+    }
 
+    @Override
+    protected void salvarAvaliacao(AvaliacaoProduto avaliacao) {
         repositorio.save(avaliacao);
     }
 
-
-    public Double mediaNotas(Long idProduto) {
-        Double media = repositorio.mediaPorProduto(idProduto);
-        return media != null ? media : 0.0;
+    @Override
+    protected List<Integer> buscarNotas(Long idProduto) {
+        return repositorio.buscarTodasNotas(idProduto);
     }
 
-    
-    public Page<AvaliacaoProduto> listarPorProduto(Long idProduto, Pageable pageable) {
+    public Page<AvaliacaoProduto> listarPorProduto(
+            Long idProduto,
+            Pageable pageable) {
+
         return repositorio.findByProdutoId(idProduto, pageable);
     }
 }
-
-
