d1e5d24dd2994224eabaa55f5796670487e05fda
Luigi Morgenstern Milani <luigimilani2003@outlook.com>
2026-09-24 21:14:01 -0300

    Update AvaliacaoProduto.java

--- projetos/back-end/src/main/java/OMCE/OMCE/AvaliacaoProduto/AvaliacaoProduto.java
+++ projetos/back-end/src/main/java/OMCE/OMCE/AvaliacaoProduto/AvaliacaoProduto.java
@@ -23,6 +23,7 @@ public class AvaliacaoProduto {
     @JoinColumn(name = "produto_id")
     private Produto produto;
 
+    @Column(name = "criado_em")
     private LocalDateTime data = LocalDateTime.now();
 
     public AvaliacaoProduto() {
